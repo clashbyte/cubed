9a293e2fae074065f9d9cea570951d941facb53f
Mikhail Popov <fatassantuan@gmail.com>
2018-06-14 14:17:11 +0300

    Editor attribute

---
+++ Editor/Data/Editor/Attributes/FileEditorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cubed.Data.Editor.Attributes {
+
+	[AttributeUsage(AttributeTargets.Class)]
+	public class FileEditorAttribute : Attribute {
+
+		public FileEditorAttribute() {
+			
+		}
+
+
+	}
+}
